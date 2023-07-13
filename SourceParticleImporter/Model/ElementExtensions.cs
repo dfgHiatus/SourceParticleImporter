b171ca6553e02d87cd34e52db53b29f7141bf891
dfgHiatus <51272212+dfgHiatus@users.noreply.github.com>
2023-07-13 10:17:40 -0600

    Addenum to prev commit

--- SourceParticleImporter/Model/ElementExtensions.cs
+++ SourceParticleImporter/Model/ElementExtensions.cs
@@ -5,7 +5,7 @@ namespace SourceParticleImporter.Model;
 
 internal static class ElementExtensions
 {
-    internal static T PCFToNeosValue<T>(this Element element, string key, T defaultValue)
+    internal static T PCFToNeosValue<T>(this Element element, string key, T defaultValue = default)
     {
         return element.TryGetValue(key, out object obj) ? (T)obj : defaultValue;
     }
